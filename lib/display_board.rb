047906d5c465203fc0f909444b62315fe789baaf
Mike Doig <mike.j.doig@gmail.com>
2019-02-19 22:58:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+board_row = [" ","|", " ", "|", " "]
+board_row_divider = ["---","-","---","-","---"]
+def display_board
+  board_row
+  board_row_divider
+  board_row
+  board_row_divider
+  board_row
+end
+display_board
