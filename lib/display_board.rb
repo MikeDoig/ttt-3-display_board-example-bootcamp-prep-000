0755bf6a853186591b66f88175ee6b938de9d494
Mike Doig <mike.j.doig@gmail.com>
2019-02-19 23:09:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,24 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  board_row = " "
-  board_column_divider = "|"
-  board_row_divider = "-----"
-  print board_row
-  print board_column_divider
-  print board_row
-  print board_column_divider
-  puts board_row
-  puts board_row_divider
-  print board_row
-  print board_column_divider
-  print board_row
-  print board_column_divider
-  puts board_row
-  puts board_row_divider
-  print board_row
-  print board_column_divider
-  print board_row
-  print board_column_divider
-  puts board_row
+  
 end
 display_board
